adcada65f399e4cc799f1ae280e609606a0d8a56
Bjarke Meyer Pedersen <bjark@DESKTOP-Q9VC94V.localdomain>
2022-09-12 15:43:50 +0200

    first try, not pretty

--- src/merge.py
+++ src/merge.py
@@ -16,7 +16,18 @@ def merge(x: list[int], y: list[int]) -> list[int]:
     # FIXME: fill out the loop so you merge the lists
     # until one of them is empty
     while i < len(x) and j < len(y):
-        break  # FIXME: you shouldn't just break here
+        if x[i]>=y[j]:
+            z.append(y[j])
+            j+=1
+        if x[i]<=y[j]:
+            z.append(x[i])
+            i+=1
+        if i==len(x):
+            z.append(*y[j:])
+            break
+        if j==len(y):
+            z.append(*x[i:])
+            # FIXME: you shouldn't just break here
     # At least one of the lists is empty now. Copy the
     # remainder of the other into z.
     return z
